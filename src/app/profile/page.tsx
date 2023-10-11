f091ac5afe89c4ae1869ca2a74d56a240dafc028
Michael Gomeh <67332401+michaelgomeh@users.noreply.github.com>
2023-10-11 22:44:42 +0300

    added profile page, db, auth

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,24 @@
 import React from "react"
 import { Stack } from "@mantine/core"
+import { Avatar, Text, Button, Paper } from "@mantine/core"
+
 const Page = () => {
-  return <Stack>asd</Stack>
+  return (
+    <Paper radius="md" withBorder p="lg" bg="var(--mantine-color-body)">
+      <Avatar
+        src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+        size={120}
+        radius={120}
+        mx="auto"
+      />
+      <Text ta="center" fz="lg" fw={500} mt="md">
+        Jane Fingerlicker
+      </Text>
+      <Text ta="center" c="dimmed" fz="sm">
+        jfingerlicker@me.io • Art director
+      </Text>
+    </Paper>
+  )
 }
 
 export default Page
